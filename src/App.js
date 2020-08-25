bfa21bdfc3225be0beac48c7396fa3ba6989721b
杜翔 <duxiang@duxiangdeMacBook-Pro.local>
2020-08-25 17:49:17 +0800

    完成首页

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { HashRouter as Router , Route } from 'react-router-dom'
 import FirstRouter from './firstRouter'
+import './style/style.scss'
 import Tabbar from './components/tabbar'
 export default class App extends Component {
   render() {
