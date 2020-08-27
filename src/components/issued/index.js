1884d042c791ed1f8ebb5f2257d71e7978ac3077
liyanwei741 <13450166620@163.com>
2020-08-27 09:38:09 +0800

    27更新

--- src/components/issued/index.js
+++ src/components/issued/index.js
@@ -5,16 +5,9 @@ import Users from '../discover/User'
 class index extends Component {
     render() {
         return (
-<<<<<<< HEAD
-                <div>
-                    <h1>发布</h1>
-                </div>  
-=======
             <div>
-                
-                
+             
             </div>
->>>>>>> b06f71111c02812b15cb610f91fbbf1ce29bebdd
         )
     }
 }
